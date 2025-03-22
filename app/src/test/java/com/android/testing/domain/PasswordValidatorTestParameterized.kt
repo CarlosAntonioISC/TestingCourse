a958460714c8520a64a29e64fc5423f4cc8423d8
Carlos Antonio <jose.antonio@elektra.com.mx>
2025-03-21 19:41:04 -0600

    add parameterized tests PasswordValidator

---
+++ app/src/test/java/com/android/testing/domain/PasswordValidatorTestParameterized.kt
@@ -0,0 +1,36 @@
+package com.android.testing.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class PasswordValidatorTestParameterized(
+    private val password: String,
+    private val expected: Boolean
+) {
+
+    private val suv = PasswordValidator()
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): List<Array<Any>> {
+            return listOf(
+                arrayOf("Password1!", true),
+                arrayOf("Pass1!", false),
+                arrayOf("PASSWORD1!", false),
+                arrayOf("password1!", false),
+                arrayOf("Password!", false),
+                arrayOf("Password123", false)
+            )
+        }
+    }
+
+   @Test
+   fun `Validate passwords`() {
+       assertEquals(expected, suv.isValid(password))
+   }
+
+}
