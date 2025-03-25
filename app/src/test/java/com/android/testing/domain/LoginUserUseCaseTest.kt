238f6a32873c58e256f123befaa36d0b27fee9b6
Carlos Antonio <jose.antonio@elektra.com.mx>
2025-03-25 13:17:00 -0600

    add example suspend function test
    - add testing coroutines library

--- app/src/test/java/com/android/testing/domain/LoginUserUseCaseTest.kt
+++ app/src/test/java/com/android/testing/domain/LoginUserUseCaseTest.kt
@@ -1,18 +1,29 @@
 package com.android.testing.domain
 
+import com.android.testing.data.FakeUserRepository
+import com.android.testing.data.StubLoginRepository
 import com.android.testing.domain.repository.LoginRepository
 import com.android.testing.domain.repository.UserRepository
 import com.android.testing.domain.usecase.LoginUserUseCase
-import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class LoginUserUseCaseTest {
 
-    private val loginRepository: LoginRepository = mockk()
-    private val userRepository: UserRepository = mockk()
+    private val loginRepository: LoginRepository = StubLoginRepository()
+    private val userRepository: UserRepository = FakeUserRepository()
     private val suv = LoginUserUseCase(
         loginRepository = loginRepository,
         userRepository = userRepository
     )
 
+    @Test
+    fun `When login return success then save user`() = runTest {
+        val userName = "test name"
+        suv.invoke(userName, "test", "test")
+        assertEquals(userName, userRepository.get()!!.name)
+    }
 
 }
