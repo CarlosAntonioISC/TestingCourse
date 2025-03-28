2b9242b9f5f26da718d26dbe7a8bc16c1e6e87c4
Carlos Antonio <jose.antonio@elektra.com.mx>
2025-03-28 10:02:51 -0600

    add example test with mocks

--- app/src/test/java/com/android/testing/domain/LoginUserUseCaseTest.kt
+++ app/src/test/java/com/android/testing/domain/LoginUserUseCaseTest.kt
@@ -1,16 +1,19 @@
 package com.android.testing.domain
 
 import com.android.testing.domain.models.Result
+import com.android.testing.domain.models.ServerError
 import com.android.testing.domain.models.User
 import com.android.testing.domain.models.UserId
 import com.android.testing.domain.repository.LoginRepository
 import com.android.testing.domain.repository.UserRepository
 import com.android.testing.domain.usecase.LoginUserUseCase
+import io.mockk.clearMocks
 import io.mockk.coEvery
 import io.mockk.coJustRun
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
 class LoginUserUseCaseTest {
@@ -22,13 +25,17 @@ class LoginUserUseCaseTest {
         userRepository = userRepository
     )
 
+    @Before
+    fun setUp() {
+        clearMocks(loginRepository, userRepository)
+    }
+
     @Test
     fun `When login return success then save user`() = runTest {
         val userId = UserId(1)
         val userName = "carlos"
         val userPassword = "password123"
         val userEmail = "carlos@gmail.com"
-
         coEvery {
             loginRepository.login(
                 userName = userName,
@@ -36,12 +43,22 @@ class LoginUserUseCaseTest {
                 userEmail = userEmail
             )
         } returns Result.Success(userId)
-
         coJustRun { userRepository.save(User(userId, userName, userEmail)) }
 
         suv.invoke(userName, userPassword, userEmail)
 
         coVerify(exactly = 1) { userRepository.save(User(userId, userName, userEmail) ) }
+    }
+
+    @Test
+    fun `When login return error then don't save user`() = runTest {
+        coEvery {
+            loginRepository.login(userName = any(), userPassword = any(), userEmail = any())
+        } returns Result.Error(ServerError)
+
+        suv.invoke("userName", "userPassword", "userEmail")
+
+        coVerify(exactly = 0) { userRepository.save(any()) }
     }
 
 }
